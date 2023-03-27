6920a6f4ea7343e5813ccd8058ea4a04877c371b
hieu20022002 <99581395+hieu20022002@users.noreply.github.com>
2023-03-27 15:38:15 +0700

    create ui profile screen (#1)
    Co-authored-by: MaiLienNguyen <128075442+MaiLienNguyen@users.noreply.github.com>

---
+++ lib/screens/profile/profile_screen.dart
@@ -0,0 +1,18 @@
+import 'package:ecommerce/components/custom_appbar.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerce/enums.dart';
+
+import 'components/body.dart';
+
+class ProfileScreen extends StatelessWidget {
+  static String routeName = "/profile";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+      ),
+      body: Body(),
+    );
+  }
+}
