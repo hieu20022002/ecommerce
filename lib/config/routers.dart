e1f685ddd522da3678a78f20b7876c3018ba1538
hieu20022002 <hieunguyen31122001@gmail.com>
2023-04-05 21:59:23 +0700

    fix code router

--- lib/config/routers.dart
+++ lib/config/routers.dart
@@ -4,12 +4,10 @@ import 'package:ecommerce/screens/home/home_screen.dart';
 import 'package:ecommerce/screens/otp/otp_screen.dart';
 import 'package:ecommerce/screens/sign_up/sign_up_screen.dart';
 import 'package:flutter/material.dart';
-
 import '../screens/complete_profile/complete_profile_screen.dart';
 import '../screens/forgot_password/forgot_password_screen.dart';
 import '../screens/login_success/login_success_screen.dart';
 import '../screens/sign_in/sign_in_screen.dart';
-
 import 'package:ecommerce/screens/profile/profile_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
@@ -23,5 +21,6 @@ final Map<String, WidgetBuilder> routes = {
   ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
   LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
   DetailsScreen.routeName: (context) => DetailsScreen(),
+  ProfileScreen.routeName: (context) => ProfileScreen(),
 };
 
--- lib/models/Category.dart
+++ lib/models/Category.dart
@@ -1,14 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Category {
-  String name, image;
+  String name;
 
-  Category({required this.name, required this.image});
+  Category({required this.name});
   factory Category.fromFirestore(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return Category(
       name: data['name'],
-      image: data['image'],
     );
   }
   List<Category> categories = [];
