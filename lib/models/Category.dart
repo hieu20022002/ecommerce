152a4ce5c05799d934fd76181a21d0f0bade3e92
hieu20022002 <hieunguyen31122001@gmail.com>
2023-04-05 22:11:29 +0700

    Update Category.dart

--- lib/models/Category.dart
+++ lib/models/Category.dart
@@ -1,14 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Category {
-  String name, image;
-
-  Category({required this.name, required this.image});
+  String name;
+  Category({required this.name});
   factory Category.fromFirestore(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return Category(
       name: data['name'],
-      image: data['image'],
     );
   }
   List<Category> categories = [];
